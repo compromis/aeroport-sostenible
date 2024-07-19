12fb0ea2b129fcf74f9ddbada9a375d3fd4d3933
Guillem <guillem@disedit.com>
2024-07-19 08:51:07 +0200

    fix fb share url

--- components/Share.vue
+++ components/Share.vue
@@ -16,7 +16,7 @@
         <a href="https://t.me/share/url?url=https://compromis.net/aeroport-sostenibl&text=" class="link telegram">
           <span>-&gt;</span> Telegram
         </a>
-        <a href="https://www.facebook.com/sharer/sharer.php?u=https://compromis.net/aeroport-sostenibl" class="link facebook">
+        <a href="https://www.facebook.com/sharer/sharer.php?u=https://compromis.net/aeroport-sostenible" class="link facebook">
           <span>-&gt;</span> Facebook
         </a>
         <a href="https://twitter.com/intent/tweet/?text=&url=https://compromis.net/aeroport-sostenibl&hashtags=AeroportSostenible" class="link twitter">
